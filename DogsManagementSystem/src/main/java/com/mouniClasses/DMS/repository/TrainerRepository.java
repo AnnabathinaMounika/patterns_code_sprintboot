f9ccfde82ded2867d036e538d0e6638f94b5bf36
AnnabathinaMounika <112028867+AnnabathinaMounika@users.noreply.github.com>
2023-04-28 00:15:09 -0500

    added sprintboot code

---
+++ DogsManagementSystem/src/main/java/com/mouniClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.mouniClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mouniClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
